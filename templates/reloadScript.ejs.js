c39781dfd2dbf15ae3aa25c837d42a73e6a1d2dc
John Henry <john@iamjohnhenry.com>
2015-03-01 00:50:41 -0800

    Refactor host to improve live reload

--- templates/reloadScript.ejs.js
+++ templates/reloadScript.ejs.js
@@ -2,22 +2,24 @@
   var reconnectInterval = 5000;
   var ws;
   var connect = function(){
-      console.log("attempting to connect...")
+      if(ws) return;
+      console.log("socket connecting...");
       ws = new WebSocket('ws'+(window.location.protocol === 'https'? 's': "")  +'://localhost:<%= _port %>/');
       ws.onopen = function() {
-        console.log("socket open")
+        console.log("socket connected.");
         //window.location.reload();
       };
       ws.onerror = function() {
+        ws = null;
         console.log('socket error. reconnecting...');
         setTimeout(connect, reconnectInterval);
       };
       ws.onclose = function() {
+        ws = null;
         console.log('socket close. reconnecting...');
         setTimeout(connect, reconnectInterval);
       };
       ws.onmessage =function(message){
-        console.log(message)
         switch(message.data){
           case 'reload': window.location.reload(); break;
         }
